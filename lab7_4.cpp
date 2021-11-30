5651fd2d9a6225af228a0229b7a8a6beaadb4f73
siksaka09 <earthsik09@gmail.com>
2021-11-30 16:51:56 +0700

    lab7_4 completed

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -8,8 +8,21 @@ int main(){
 	string name[] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
+	double x;
 
-    //Write your code here.
+    cout << "Please input GPA: ";
+	cin >> x;
+	int Araysize = sizeof(id)/sizeof(id[0]);
+	int i = 0;
+	while (i < Araysize)
+	{
+		if (gpa[i] >= x)
+		{
+			cout << id[i] << " " << name[i] << " " << gpa[i] << endl;
+		}
+
+		i++;
+	}
 		
 	return 0;
 }
